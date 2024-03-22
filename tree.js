04e820fc6feb6eedb099ca6a30c14dbc2e2efb84
Jared <jared_glenn@yahoo.com>
2024-03-21 22:27:20 -0600

    add tree.js; passing all tests

--- tree.js
+++ tree.js
@@ -15,21 +15,72 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) {
+      return 0;
+    }
+    const numList = [this.root];
+    let sum = 0;
+
+    while (numList.length > 0) {
+      let treeNode = numList.shift()
+
+      for (let child of treeNode.children) {
+        numList.push(child);
+      }
+
+      sum += treeNode.val;
+    }
+
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) {
+      return 0;
+    }
+    const numList = [this.root];
+    let res = 0;
 
+    while (numList.length > 0) {
+      let treeNode = numList.shift()
+
+      for (let child of treeNode.children) {
+        numList.push(child);
+      }
+
+      if (treeNode.val%2 === 0) {
+        res++;
+      }
+    }
+
+    return res;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) {
+      return 0;
+    }
+    const numList = [this.root];
+    let res = 0;
 
-  }
+    while (numList.length > 0) {
+      let treeNode = numList.shift()
+
+      for (let child of treeNode.children) {
+        numList.push(child);
+      }
+
+      if (treeNode.val > lowerBound) {
+        res++;
+      }
+    };
+    return res;
+}
 }
 
 module.exports = { Tree, TreeNode };
